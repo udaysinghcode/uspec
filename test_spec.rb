8c4e44456d958f11c2ad7d009c26bb28a0272ae1
Uday Singh <udaysingh.code@gmail.com>
2018-04-10 04:56:55 -0400

    Added test spec for building uspec

---
+++ test_spec.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require_relative 'lib/spec'
+
+class TestDesribe < Test::Unit::TestCase
+  def test_that_it_can_pass
+    describe 'some thing' do
+      it 'has some property' do
+      end
+    end
+  end
+
+  def test_that_it_can_fail
+    assert_raise(IndexError) do
+      describe 'some failing thing' do
+        it 'fails' do
+          raise IndexError
+        end
+      end
+    end
+  end
+end
+
+class TestAssertion < Test::Unit::TestCase
+  def test_that_it_can_pass
+    2.should == 2
+  end
+
+  def test_that_it_can_fail
+    assert_raise(AssertionError) do
+      1.should == 2
+    end
+  end
+end
